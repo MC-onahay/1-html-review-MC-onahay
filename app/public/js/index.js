f58d0b4c1c41c399382a2a2000167164efcaea99
MC-onahay <89480663+MC-onahay@users.noreply.github.com>
2021-09-20 09:58:44 -0400

    fix nav bar with bootstrap links

---
+++ app/public/js/index.js
@@ -0,0 +1,31 @@
+const Rando = {
+    
+    computed:{
+        prettyBirthday(){
+            return dayjs(this.person.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+    methods:{
+        fetchUserData(){
+            console.log("A");
+        fetch('https://randomuser.me/api/')
+        .then( response => response.json() )
+        .then( (responseJson) => {
+            console.log(responseJson);
+            console.log("C");
+            this.person = responseJson.results[0];
+        })
+        .catch( (err) => {
+            console.error(err);
+        })
+        console.log("B");
+        }
+    },
+    created() {
+        this.fetchUserData();
+    } //end created
+} // end Offer config
+  
+Vue.createApp(Rando).mount('#randoApp');
+console.log("Z");
