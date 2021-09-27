8a440f4b72725d35b74812ba63977d40f62894e1
MC-onahay <89480663+MC-onahay@users.noreply.github.com>
2021-09-27 13:31:08 -0400

    Update test.php

--- app/public/api/test.php
+++ app/public/api/test.php
@@ -2,8 +2,59 @@
 
 $num = 2;
 $foo = "To be";
-$bar = "or not ".$num." be";
+$bar = "or not " .$num. " be";
 
-echo $foo ." ". $bar;
+echo $foo . " " . $bar . "\n";
 
 echo $num * $num * $num;
+
+$arr = [
+    "first" => "Tom",
+    "second" => "Bipin",
+    "best" => "DS"
+];
+
+$arr2 = [1,1,2,3,5,8];
+
+
+if (true) {
+    echo "\nTRUE\n";
+}
+
+while (true) {
+    //This way it doesn't actually do anything
+    break;
+}
+
+# This is also a comment
+
+/* This is a 
+multi-line 
+comment */
+
+function printlist($somArr) {
+    echo "<ul>\n";
+    foreach($arr as $key=>$val) {
+        echo "<li>".$key ." is ".$val."</li>\n";
+    }
+    echo "</ul\n>";
+}
+
+printList($arr);
+printList($arr2);
+
+function printAndEncode( $val ) {
+    echo json_encode(
+        $val, 
+        JSON_PRETTY_PRINT|JSON_THROW_ON_ERROR
+    );
+}
+
+// ====
+// Naming conventions
+
+// JS & PHP : camelCase
+
+// PascalCase
+// snake_case
+// kebab-case
